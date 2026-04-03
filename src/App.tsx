535055de042dde59a8aeee07dc6078d568f0d07c
atxprestigedetailing-design <atxprestigedetailing@gmail.com>
2026-04-03 12:13:15 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useMemo, useState, useEffect } from "react";
 import logo from "./assets/logo.png";
 const SCRIPT_URL =
-   "https://script.google.com/macros/s/AKfycbznFEIw_qvs5omuKWfP-b6CroaZuYhUSQa4lD2ARGcXBZ8HE_j6LmvYquDKaqocoFIBxA/exec";
+   "https://script.google.com/macros/s/AKfycbzfMX9a096ix9jNQFsLY6nsIZ41i6tsAnt3ZwHQuuEGNho3rdMP1Xi_0r79Xmfn16ssxA/exec";
    type AvailabilitySlot = {
   date: string;
   time: string;
