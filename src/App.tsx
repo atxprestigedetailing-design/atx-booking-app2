d35bbd290579b9696543eb4e0e6e8b9f5bb8f728
atxprestigedetailing-design <atxprestigedetailing@gmail.com>
2026-05-18 13:01:56 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const GOOGLE_CLIENT_ID =
   "447699234633-ivo2e1c2q843scj32k5323o2rkq6h7dp.apps.googleusercontent.com";
 
 const SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbytLuNWR4QH9hKKFrGbwuFP3XsLU9r4daZn4XT0A4qOlJ5tjOZ2R0vE4gSuyhpWiUg5xA/exec";
+  "https://script.google.com/macros/s/AKfycbwgnwzxFP_e11m_LYBsrIzWdSKXDcGzcohC2NdiL6ZD3wimJoAmkEmrDNejEdBZAMS9/exec";
 
 const TOTAL_STEPS = 9;
 const ADMIN_EMAIL = "atxprestigedetailing@gmail.com";
